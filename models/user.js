0388c5053805edc4eafb2b1875163995b6cd06ba
Mehdi Loisel <spikeze@gmail.com>
2017-06-15 15:41:16 +0200

    initialisation user

---
+++ models/user.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        nom: DataTypes.STRING,
+        prenom: DataTypes.STRING,
+        login: {type:DataTypes.STRING,unique: 'compositeIndex'},
+        password: DataTypes.STRING,
+        isadmin: DataTypes.BOOLEAN
+    }, {
+        classMethods: {
+            associate: function(models) {
+                //les has many
+            }
+        }
+    });
+
+    return User;
+};
